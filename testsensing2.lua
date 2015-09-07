aabf34894f5b40539fcb585959bfc2e80ded0ce9
wordsforthewise <wordsforthewise@gmail.com>
2015-09-07 09:53:34 -0600

    add 5 levels

--- testsensing2.lua
+++ testsensing2.lua
@@ -1,4 +1,22 @@
-gpio.mode(1,gpio.INPUT)
+local pin1 = 1
+local pin2 = 2
+local pin3 = 5
+local pin4 = 6
+local pin5 = 7
+gpio.mode(pin1, gpio.INPUT)
+gpio.mode(pin2, gpio.INPUT)
+gpio.mode(pin3, gpio.INPUT)
+gpio.mode(pin4, gpio.INPUT)
+gpio.mode(pin5, gpio.INPUT)
 tmr.alarm(1,1000,1,function()
-    print(gpio.read(1))
+    print('1 ft:')
+    print(gpio.read(pin1))
+    print('2 ft:')
+    print(gpio.read(pin2))
+    print('3 ft:')
+    print(gpio.read(pin3))
+    print('4 ft:')
+    print(gpio.read(pin4))
+    print('5 ft:')
+    print(gpio.read(pin5))
 end)

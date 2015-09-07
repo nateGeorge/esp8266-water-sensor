06e4c053da952fa5e81505570c22939987f54abc
wordsforthewise <wordsforthewise@gmail.com>
2015-09-07 09:53:11 -0600

    add 5 feet of water levels

--- logdata.lua
+++ logdata.lua
@@ -1,7 +1,27 @@
-gpio.mode(1,gpio.INPUT) 
-dataToSend[1] = {'Water Full', gpio.read(1)}
-print(gpio.read(1))
+-- initialize pins for reading water levels of 1-5 ft
+-- table is in format pin number, feet
+local waterLevels = {}
+local dataToSend = {}
+local waterHeight
+local lastNum
+waterLevels[1] = {1, 1}
+waterLevels[2] = {2, 2}
+waterLevels[3] = {5, 3}
+waterLevels[4] = {6, 4}
+waterLevels[5] = {7, 5}
 
+for num, arr in ipairs(waterLevels) do
+    gpio.mode(arr[1], gpio.INPUT)
+    dataToSend[num] = {tostring(arr[2]), gpio.read(arr[1])}
+    if gpio.read(arr[1]) == 1 then
+        waterHeight = arr[2];
+    end
+    lastNum = num
+end
+if waterHeight == nil then
+    waterHeight = 0
+end
+dataToSend[lastNum + 1] = {'Water Height', waterHeight}
 sendToTS = require("sendToTS")
 sendToTS.sendData(dataToSend, true)
 

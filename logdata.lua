2b0d38482ef60aac52b0095d89ca7b076e5d886e
wordsforthewise <wordsforthewise@gmail.com>
2015-09-08 17:45:18 -0600

    add logic to keep checking if tank full, if filling

--- logdata.lua
+++ logdata.lua
@@ -9,7 +9,6 @@ waterLevels[2] = {2, 2}
 waterLevels[3] = {5, 3}
 waterLevels[4] = {6, 4}
 waterLevels[5] = {7, 5}
-
 for num, arr in ipairs(waterLevels) do
     gpio.mode(arr[1], gpio.INPUT)
     dataToSend[num] = {tostring(arr[2]), gpio.read(arr[1])}
@@ -18,17 +17,30 @@ for num, arr in ipairs(waterLevels) do
     end
     lastNum = num
 end
-if waterHeight == nil then
-    waterHeight = 0
-end
-dataToSend[lastNum + 1] = {'Water Height', waterHeight}
-if waterHeight == 5 then
-    dataToSend[lastNum + 2] = {'Water Full', 1}
-else
-    dataToSend[lastNum + 2] = {'Water Full', 0}
-end
-sendToTS = require("sendToTS")
-sendToTS.sendData(dataToSend, false, true)
-
-sendToTS = nil
-package.loaded["sendToTS"]=nil
+tmr.alarm(4, 10*1000, 1, function()
+    for num, arr in ipairs(waterLevels) do
+        gpio.mode(arr[1], gpio.INPUT)
+        dataToSend[num] = {tostring(arr[2]), gpio.read(arr[1])}
+        if gpio.read(arr[1]) == 1 then
+            waterHeight = arr[2];
+        end
+        lastNum = num
+    end
+    if waterHeight == nil then
+        waterHeight = 0
+    end
+    dataToSend[lastNum + 1] = {'Water Height', waterHeight}
+    if waterHeight >= 4 then
+        dataToSend[lastNum + 2] = {'Water Full', 1}
+    else
+        dataToSend[lastNum + 2] = {'Water Full', 0}
+    end
+    sendToTS = require("sendToTS")
+    sendToTS.sendData('JSF water level keys', dataToSend, false, true)
+    sendToTS = nil
+    package.loaded["sendToTS"]=nil
+    if waterHeight >= 4 then
+        tmr.stop(4)
+        node.dsleep(5*60*1000*1000) -- deep sleep for 5 minutes
+    end
+end)

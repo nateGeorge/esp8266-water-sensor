6e9abb83699b9408a888c46949da42a4a5ea3ba0
wordsforthewise <wordsforthewise@gmail.com>
2015-09-10 15:38:32 -0600

    dont write relay low, change keys file name

--- managepump2.lua
+++ managepump2.lua
@@ -1,8 +1,7 @@
-relayPin = 1
+relayPin = 2
 gpio.mode(relayPin, gpio.OUTPUT)
-gpio.write(relayPin, gpio.LOW)
 
 readTS = require("readTS")
-readTS.readData('JSF water level keys', true, 1, 'checkPumpSettings.lua')
+readTS.readData('JSF water pump settings', true, 1, 'checkPumpSettings.lua')
 readTS = nil
 package.loaded["readTS"]=nil

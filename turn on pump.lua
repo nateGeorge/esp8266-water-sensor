58460905ba90f79e73613aa2a0729a5d77c2dcbf
wordsforthewise <wordsforthewise@gmail.com>
2015-09-10 15:39:27 -0600

    change timing structure--2 timers don't work at once

--- turn on pump.lua
+++ turn on pump.lua
@@ -1,26 +1,39 @@
-tmr.alarm(5, timeToOff*60*1000*1000, 0, function()
-    tmr.stop(6)
+dataToSend = {}
+dataToSend[1] = {"pump on", 1}
+sendToTS = require("sendToTS")
+sendToTS.sendData("JSF pump keys", dataToSend, false, true)
+sendToTS = nil
+package.loaded["sendToTS"]=nil
+startTime = tmr.now()/(1000*1000)
+gpio.write(relayPin, gpio.HIGH)
+shutOff = timeToOff*60
+print('time to shutoff: '..tostring(shutOff))
+
+local function stopPump()
+    print('stopping pump because max time reached')
+    gpio.write(relayPin, gpio.LOW)
     dataToSend[1] = {"pump on", 0}
     sendToTS = require("sendToTS")
     sendToTS.sendData("JSF pump keys", dataToSend, false, true, 'pauseAndRestart.lua')
     sendToTS = nil
     package.loaded["sendToTS"]=nil
-end)
+end
 
-gpio.write(relayPin, gpio.HIGH)
-
-tmr.alarm(6, 10*1000*1000, 1, function()
-    dataToSend = {}
-    dataToSend[1] = {"pump on", 1}
-    sendToTS = require("sendToTS")
-    sendToTS.sendData("JSF pump keys", dataToSend, false, true)
-    sendToTS = nil
-    package.loaded["sendToTS"]=nil
-    if not manualRun then
+tmr.alarm(2, 10*1000, 0, function()
+    print('checking pump settings, then water height')
+    if ((tmr.now()/(1000*1000)-startTime) >= shutOff) then
+        stopPump()
+    elseif not manualRun then
         -- check the water height and shut off pump if tanks are full
         readTS = require("readTS")
-        readTS.readData('JSF water level keys', true, 1, 'compareHeight.lua')
+        readTS.readData('JSF water level keys', true, 1, 'managepump2.lua')
         readTS = nil
         package.loaded["readTS"]=nil
+    else
+        tmr.alarm(3, 10*1000, 1, function()
+            if ((tmr.now()/(1000*1000)-startTime) >= shutOff) then
+                stopPump()
+            end
+        end)
     end
 end)

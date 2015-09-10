760f64829d870ce0496b7e9364fbe730fe837af1
wordsforthewise <wordsforthewise@gmail.com>
2015-09-10 08:39:15 -0600

    convert str to num, remove nl

--- compareHeight.lua
+++ compareHeight.lua
@@ -1,4 +1,5 @@
-if fields[6]>=maxHeight then
+-- if we have reached the cutoff height for stopping the pump, turn off the pump and wait for next reading
+if tonumber(fields[6])>=maxHeight then
     tmr.stop(6)
     sendToTS = require("sendToTS")
     sendToTS.sendData("JSF pump keys", {"pump on", 0}, false, true, 'pauseAndRestart.lua')
